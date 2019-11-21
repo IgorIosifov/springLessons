9379b6b7d8fd2e78f2b036a70fbf5e83a35c95d1
IgorIosifov <igoriosifov@yahoo.com>
2019-11-21 22:52:35 +0300

    Homework for lesson 1

---
+++ src/main/java/com/geekbrains/spring/MarketConfig.java
@@ -0,0 +1,12 @@
+package com.geekbrains.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(value = "com.geekbrains.spring")
+public class MarketConfig {
+
+
+}
---
+++ src/main/java/com/geekbrains/spring/ProductRepository.java
@@ -0,0 +1,42 @@
+package com.geekbrains.spring;
+
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    private List<Product> products;
+
+    @PostConstruct
+    public void init() {
+        products = new ArrayList<>();
+        products.add(new Product(1L, "Bread", new BigDecimal(15.0)));
+        products.add(new Product(2L, "Coffee", new BigDecimal(80.0)));
+        products.add(new Product(3L, "Onion", new BigDecimal(5.0)));
+    }
+
+    public List<Product> getAllProducts() {
+        return Collections.unmodifiableList(products);
+    }
+
+    public void insert(Product product) {
+        products.add(product);
+    }
+
+    public Product getProductByID(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+}
+
