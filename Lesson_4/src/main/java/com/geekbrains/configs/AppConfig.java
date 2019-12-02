3399254255a41521221e8d939b40e3f5feedc2e6
IgorIosifov <igoriosifov@yahoo.com>
2019-12-02 14:13:36 +0300

    Homework for lesson 4. Base project.

---
+++ Lesson_4/src/main/java/com/geekbrains/configs/AppConfig.java
@@ -0,0 +1,9 @@
+package com.geekbrains.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.geekbrains"})
+public class AppConfig {
+}
